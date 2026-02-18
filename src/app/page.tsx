caef68a8ce76980c8a67ad9702fdd9e0ef794f4b
Ricardo Carmo <therick0156@gmail.com>
2026-02-18 19:57:22 -0300

    Editando o arquivo Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,43 @@
 import { Button } from "../components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Card } from "../components/ui/card";
+import { Card, CardContent, CardHeader } from "../components/ui/card";
 import { Separator } from "../components/ui/separator";
-import { Plus, List, Check, BadgeAlert } from 'lucide-react';
+import { Plus, List, Check, BadgeAlert, SquarePen, Trash  } from 'lucide-react';
 import { Badge } from "../components/ui/badge";
 
 const Home = () => {
   return (
     <main className="w-full h-screen bg-gray-100 flex justify-center items-center"> 
-      <Card className="w-lg p-4">
-        <div className="flex gap-2">
+      <Card className="w-lg">
+
+        <CardHeader className="flex gap-2">
           <Input placeholder="Adicionar tarefa"/>
-          <Button variant="outline" className="cursor-pointer"> <Plus/>Cadastrar</Button> {/* muda o botão para um estilo claro com letra preta */}
-        </div>
+          <Button variant="default" className="cursor-pointer"> <Plus/>Cadastrar</Button> {/* muda o botão para um estilo claro com letra preta */}
+        </CardHeader>
 
-        {<Separator/>}
+       
+        <CardContent>
+          <Separator className="mb-4"/>
+          <div className="flex gap-2">
+            <Badge className="cursor-pointer"> <List/>Todas</Badge>
+            <Badge className="cursor-pointer"> <BadgeAlert/>Não finalizadas</Badge>
+            <Badge className="cursor-pointer"> <Check/>Concluidas</Badge>
+          </div>
 
-        <div className="flex gap-2">
-          <Badge className="cursor-pointer"> <List/>Todas</Badge>
-          <Badge className="cursor-pointer"> <BadgeAlert/>Não finalizadas</Badge>
-          <Badge className="cursor-pointer"> <Check/>Concluidas</Badge>
+          <div className="mt-4">
+            <div className="h-12 flex justify-between items-center">
+              <div className="w-1 h-full bg-green-300"></div>
+              <p className="flex-1 px-2">Estudar React</p>
 
-        </div>
+              <div className="flex items-center">
+                <SquarePen/>
+                <Trash/>
+              </div>
+              
+            </div>
+          </div>
+        </CardContent>
+        
 
       </Card>
     </main>
